3dd0901d4b6cade4fe9c9f668ce332bc948a21dc
MeepleMeph <auvray28@gmail.com>
2015-09-13 19:53:51 +0200

    Mock : ajout client

--- Android/src/com/meeple/cloud/hivernage/db/DBMock.java
+++ Android/src/com/meeple/cloud/hivernage/db/DBMock.java
@@ -3,6 +3,8 @@ package com.meeple.cloud.hivernage.db;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import android.location.Address;
+
 import com.meeple.cloud.hivernage.model.Camping;
 import com.meeple.cloud.hivernage.model.Caravane;
 import com.meeple.cloud.hivernage.model.Client;
@@ -12,6 +14,7 @@ import com.meeple.cloud.hivernage.model.Gabarit;
 import com.meeple.cloud.hivernage.model.Hangar;
 import com.meeple.cloud.hivernage.model.Hivernage;
 import com.meeple.cloud.hivernage.model.Tranche;
+import com.meeple.cloud.hivernage.model.enums.HivernageStatus;
 import com.meeple.cloud.hivernage.service.Services;
 
 public class DBMock {
@@ -70,14 +73,29 @@ public class DBMock {
 		
 		Caravane caravane = new Caravane("AB213CD", "un peu grosse", gabarits.get(2), null);
 		Client c1 = new Client("Dupont", "Pierre", "il habite la", "ducon@gmail.com", "c'est un autiste", caravane);
+		c1.addHivernage(new Hivernage(HivernageStatus.IMPAYE, -250));
 		caravane.setClient(c1);
 		Services.clientService.create(c1);
 
 		Caravane caravane2 = new Caravane("GB852AD", "un peu grosse", gabarits.get(1), null);
 		Client c2 = new Client("LeBolosse", "Jacky", "il habite la aussi", "connard@gmail.com", "c'est un autiste", caravane2);
+		c2.addHivernage(new Hivernage(HivernageStatus.ACCOMPTE, 250));
 		caravane2.setClient(c2);
 
 		Services.clientService.create(c2);
+		
+		Caravane caravane3 = new Caravane("AF964SF", "WTF", gabarits.get(1), null);
+		Client c3 = new Client("Morane", "Bob", "1 rue des la corniche, 06560 Valbonne", "bob_morane@gmail.com", "le heros de tous les temps", caravane3);
+		c3.addHivernage(new Hivernage(HivernageStatus.IMPAYE, -159));
+		caravane3.setClient(c3);
+		Services.clientService.create(c3);
+		
+		Caravane caravane4 = new Caravane("9247VM28", "Mine!", gabarits.get(2), null);
+		Client c4 = new Client("Auvray", "François", "50 rue François Lépine, 28600 Luisant", "auvray@gmail.com", "Coucou!", caravane4);
+		c1.addHivernage(new Hivernage(HivernageStatus.PAYE, 0));
+		caravane4.setClient(c4);
+		Services.clientService.create(c4);
+		
 		
 		Services.hangarService.createHangar(new Hangar("hangar 1", 50, 20));
 		Services.hangarService.createHangar(new Hangar("hangar 2", 20, 20));
