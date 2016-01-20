afff18fb256d220f11e7aa9acf2c11b50445efd9
MeepleMeph <auvray28@gmail.com>
2016-01-21 00:19:48 +0100

    debut affichage caravanes infos

--- Android/src/com/meeple/cloud/hivernage/view/camping/CampingInfoFragment.java
+++ Android/src/com/meeple/cloud/hivernage/view/camping/CampingInfoFragment.java
@@ -5,15 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.meeple.cloud.hivernage.R;
-import com.meeple.cloud.hivernage.view.clients.ClientInfoFragment;
+import com.meeple.cloud.hivernage.model.Camping;
+import com.meeple.cloud.hivernage.service.Services;
 
 public class CampingInfoFragment extends Fragment {
 
-	public static ClientInfoFragment newInstance(int campingId) {
+	private Camping camping;
+	
+	private TextView camping_name, camping_phone, camping_mail, camping_info;
+	private ListView caravanes_liste;
+	
+	public static CampingInfoFragment newInstance(int campingId) {
 		
-		ClientInfoFragment myFragment = new ClientInfoFragment();
+		CampingInfoFragment myFragment = new CampingInfoFragment();
 		
 		Bundle args = new Bundle();
 	    args.putInt("campingId", campingId);
@@ -21,6 +29,8 @@ public class CampingInfoFragment extends Fragment {
 
 		return myFragment;
 	}
+	
+	
 	
 	
     @Override
@@ -33,9 +43,40 @@ public class CampingInfoFragment extends Fragment {
         if (savedInstanceState != null) {
 //            mCurrentPosition = savedInstanceState.getInt(ARG_POSITION);
         }
+        
+        camping = Services.campingService.findCampingById(getArguments().getInt("campingId", 0));
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.camping_info_layout, container, false);
-    } 
+        View v = inflater.inflate(R.layout.camping_info_layout, container, false); 
+        
+        initView(v);
+
+        fillCampingInformation(camping);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+	private void initView(View v) {
+		camping_name = (TextView) v.findViewById(R.id.camping_info_name);
+		camping_phone = (TextView) v.findViewById(R.id.camping_phone);
+		camping_mail = (TextView) v.findViewById(R.id.camping_mail);
+		camping_info = (TextView) v.findViewById(R.id.camping_details);
+		
+		caravanes_liste = (ListView) v.findViewById(R.id.camping_liste_caravanes_inside);
+		
+	} 
+	
+
+	private void fillCampingInformation(Camping camping2) {
+
+		camping_name.setText(camping2.getNom());
+		camping_phone.setText(camping2.getTel());
+		camping_mail.setText(camping2.getMail());
+		camping_info.setText("Prix : " + camping2.getPrix());
+		
+		
+	}
+
 
 }
