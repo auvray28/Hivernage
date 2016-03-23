b374b8cedcf9b0de819de582a22c88a8d0d30883
MeepleMeph <auvray28@gmail.com>
2016-03-23 22:00:56 +0100

    remplissage newCampingFragment (un camping n'a pas d'adresse ????)

--- Android/src/com/meeple/cloud/hivernage/view/camping/NewCampingFragment.java
+++ Android/src/com/meeple/cloud/hivernage/view/camping/NewCampingFragment.java
@@ -1,15 +1,20 @@
 package com.meeple.cloud.hivernage.view.camping;
 
 import android.app.Activity;
+import android.app.Service;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.meeple.cloud.hivernage.R;
+import com.meeple.cloud.hivernage.model.Camping;
+import com.meeple.cloud.hivernage.service.Services;
 
 public class NewCampingFragment extends Fragment {
 
@@ -59,11 +64,63 @@ public class NewCampingFragment extends Fragment {
 		edt_obs     = (EditText) v.findViewById(R.id.edt_observation);
 		
 		btn_createCamping = (Button) v.findViewById(R.id.btn_createCamping);
+		
+		btn_createCamping.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if (isAllEditFill()) {
+					createCamping();
+					Toast.makeText(getActivity(), R.string.confirmNewCamping, Toast.LENGTH_SHORT).show();
+					if(mCallback != null) mCallback.refreshList();
+					emptyAllEdt();
+				}
+				else {
+					Toast.makeText(getActivity(), R.string.fillAll, Toast.LENGTH_SHORT).show();
+				}				
+			}
+		});
 	}
 	
 	
 	
-	 @Override
+	 protected void createCamping() {
+		String nom     = edt_nom.getText().toString();
+		String adresse = edt_adresse.getText().toString();
+		String mail    = edt_mail.getText().toString();
+		String tel     = edt_tel.getText().toString();
+		Double prix    = Double.parseDouble(edt_prix.getText().toString());
+//		String obs     = edt_obs.getText().toString();
+		 
+		 Camping camp = new Camping(nom, mail, tel, prix);
+		 
+		 Services.campingService.createCamping(camp);
+	}
+
+	protected void emptyAllEdt() {
+		edt_nom.getText().clear();
+		edt_adresse.getText().clear();
+		edt_tel.getText().clear();
+		edt_mail.getText().clear();
+		edt_prix.getText().clear();
+		edt_obs.getText().clear();
+	}
+
+	protected boolean isAllEditFill() {
+
+		if( (edt_nom.getText().toString().length() == 0)  
+				|| (edt_adresse.getText().toString().length() == 0)
+				|| (edt_mail.getText().toString().length() == 0)
+				|| (edt_tel.getText().toString().length() == 0)
+				|| (edt_prix.getText().toString().length() == 0)) {
+			return false;
+		}
+
+
+		return true;
+	}
+
+	@Override
    public void onAttach(Activity activity) {
        super.onAttach(activity);
 
