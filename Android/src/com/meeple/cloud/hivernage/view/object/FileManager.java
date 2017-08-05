1104228cd209cf91c620c7c3c27a253913cca722
MeepleMeph <auvray28@gmail.com>
2017-08-05 20:44:04 +0200

    fichier FileManager oublie depot precendent

--- Android/src/com/meeple/cloud/hivernage/view/object/FileManager.java
+++ Android/src/com/meeple/cloud/hivernage/view/object/FileManager.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -27,8 +28,8 @@ import android.widget.Toast;
 
 public class FileManager {
 
-	private final static String CAMPINGS_FILENAME = "campings.csv";
-	private final static String CLIENTS_FILENAME = "clients.csv";
+	private final static String CAMPINGS_FILENAME = "Campings.csv";
+	private final static String CLIENTS_FILENAME = "Clients.csv";
 
 	private static File documents = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
 
@@ -36,7 +37,7 @@ public class FileManager {
 	public static void readCampingsCSV(Context context) {
 
 		//Get the text file
-		File file = new File(documents, CAMPINGS_FILENAME);
+		File file = new File(documents,  CAMPINGS_FILENAME);
 		int count = 0;
 
 		try {
@@ -52,9 +53,14 @@ public class FileManager {
 					if (campingsLine.length == 5) {
 						double accompte = 0;
 
-						accompte = Double.parseDouble(campingsLine[3]);
+						try {
+							accompte = Double.parseDouble(campingsLine[3]);
+						}
+						catch (Exception e) {
+							accompte = 0;
+						}
 
-						Camping ca = new Camping(campingsLine[0], campingsLine[1], campingsLine[2], accompte, campingsLine[4]);
+						Camping ca = new Camping(campingsLine[0], campingsLine[2], campingsLine[1], accompte, campingsLine[4]);
 
 						Services.campingService.createCamping(ca);
 					}
@@ -75,7 +81,7 @@ public class FileManager {
 	public static void readClientsCSV(Context context) {
 
 		//Get the text file
-		File file = new File(documents, CLIENTS_FILENAME);
+		File file = new File(documents,  CLIENTS_FILENAME);
 		int count = 0;
 
 		try {
@@ -98,20 +104,30 @@ public class FileManager {
 						client = Services.clientService.findByName(clientLine[0], clientLine[1]);
 
 						if (client == null) {
-							client = new Client(clientLine[0], clientLine[1], clientLine[2], clientLine[3], clientLine[4], clientLine[6], null);
+							int hivernage = 0;
+							client = new Client(clientLine[0], clientLine[1], clientLine[2], clientLine[4], clientLine[5], clientLine[6], null);
 
-							client.addHivernage(new Hivernage(Integer.parseInt(clientLine[5])));
+							try {
+								hivernage = Integer.parseInt(clientLine[5]);
+							}
+							catch (NumberFormatException e) { hivernage = 0; }
+
+							
+							client.addHivernage(new Hivernage(hivernage));
 
 							Services.clientService.create(client);
 						}
 					}
-					if (clientLine.length == 14 && client != null) {
+					if (client != null) {
+						String obs = "";
 						//Immatriculation;Gabarit;Camping/HIVERNAGE;Emplacement/Hangar;Entrée/X;Sortie/Y;-/Angle;Observation
 
 						Gabarit gabarit = Services.gabaritService.findGabaritByName("g"+clientLine[8]);
 						if (gabarit == null )  gabarit = Services.gabaritService.getAllGabarits().get(0);
 
-						caravane = new Caravane(clientLine[7], clientLine[14], gabarit, null);
+						if (clientLine.length > 14) obs = clientLine[14];
+						
+						caravane = new Caravane(clientLine[7], obs, gabarit, null);
 
 						// Hangar
 						//
@@ -154,7 +170,7 @@ public class FileManager {
 								Services.campingService.createCamping(camping);
 							}
 							
-							EmplacementCamping emplCamp = new EmplacementCamping(clientLine[10], caravane);
+							EmplacementCamping emplCamp = new EmplacementCamping(camping, clientLine[10], caravane);
 							try {
 								long entree = Long.parseLong(clientLine[11]);
 								emplCamp.setEntree(new Date(entree));
@@ -199,15 +215,15 @@ public class FileManager {
 			sb.append(camp.getCSVString());
 		}
 
-		File campings_file = new File(documents, CAMPINGS_FILENAME);
-
+		File campings_file = new File(documents,  CAMPINGS_FILENAME);
+		
+		
 		
 		try {
-			OutputStream os = new FileOutputStream(campings_file);
+			FileWriter filewriter = new FileWriter(campings_file);
 
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os);
-			outputStreamWriter.write(sb.toString());
-			outputStreamWriter.close();
+			filewriter.write(sb.toString());
+			filewriter.close();
 
 			Toast.makeText(context, CAMPINGS_FILENAME + " write success", Toast.LENGTH_SHORT).show();
 		}
@@ -232,14 +248,15 @@ public class FileManager {
 			sb.append(client.getCSVString());
 		}
 
-		File campings_file = new File(documents, CLIENTS_FILENAME);
+		File clients_file = new File(documents,  CLIENTS_FILENAME);
 
 		try {
-			OutputStream os = new FileOutputStream(campings_file);
 
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os);
-			outputStreamWriter.write(sb.toString());
-			outputStreamWriter.close();
+			// /storage/emulated/0/Documents/clients.csv
+			FileWriter filewriter = new FileWriter(clients_file);
+
+			filewriter.write(sb.toString());
+			filewriter.close();
 
 			Toast.makeText(context, CAMPINGS_FILENAME + " write success", Toast.LENGTH_SHORT).show();
 		}
